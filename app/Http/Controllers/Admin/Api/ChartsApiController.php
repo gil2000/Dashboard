074634f8985658f3cc09f8bc61d459f2df2c89e3
gil2000 <gil.aguilar2000@hotmail.com>
2022-04-07 23:12:41 +0100

    cenas

---
+++ app/Http/Controllers/Admin/Api/ChartsApiController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Temperatura;
+use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\Types\String_;
+use Illuminate\Database\Query\Builder;
+
+class ChartsApiController extends Controller
+{
+    public static function index(){
+
+        //$valor = Temperatura::latest()->take(10)->get()->sortBy('id');
+
+        // $valor = DB::select("SELECT * FROM (SELECT * FROM dht22_temperature_data ORDER BY Date_n_Time DESC LIMIT 10) a ORDER BY Date_n_Time");
+
+        $valor = DB::table('dht22_temperature_data')
+                    ->latest()
+                    ->limit(10)
+                    ->get()
+                    ->pluck('Temperature')
+                    ->toArray();
+
+        //$temperatura = $valor->pluck('Temperature');
+
+
+
+
+
+        return response($valor);
+
+
+
+    }
+}
